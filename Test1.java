2254237791dd567d1d21c6fc0fed9cedce110b66
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-05-22 13:23:24 +0530

    updated

---
+++ Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    int a=100;
+    int b=200;
+    static int c=300;
+    public static void main(String[] args) {
+        Test1 t1=new Test1();
+
+    }
+}
---
+++ employee.java
@@ -0,0 +1,10 @@
+public class employee {
+    int a =100;
+    int b=200;
+    public static void main(String[] args) {
+        employee e1=new employee();
+        System.out.println(e1.a);
+        System.out.println(e1.b);
+        System.out.println(e1);
+    }
+}
