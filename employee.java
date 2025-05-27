3bf923c074866f8e996bc1771b99fc7ef624b186
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-05-27 21:32:25 +0530

    updated

--- employee.java
+++ employee.java
@@ -3,8 +3,12 @@ public class employee {
     int b=200;
     public static void main(String[] args) {
         employee e1=new employee();
+        employee e2=new employee();
         System.out.println(e1.a);
         System.out.println(e1.b);
         System.out.println(e1);
+        System.out.println(e2.a);
+        System.out.println(e2.b);
+        
     }
 }
