ac013533466dd3dc9e5b9ccbf5211603849ebfe2
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-06-03 11:48:18 +0530

    updated

--- Test1.java
+++ Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
     static int c=300;
     public static void main(String[] args) {
         Test1 t1=new Test1();
+        System.out.println(t1);
 
     }
 }
---
+++ ex1.java
@@ -0,0 +1,10 @@
+public class ex1 {
+    static int num=123;
+    public static void main(String[] args) {
+        if (num >= 100 && num <= 999) {
+            System.out.println(num + " is a 3-digit number");
+        } else {
+            System.out.println(num + " is not a 3-digit number");
+        }
+    }
+}
