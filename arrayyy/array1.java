9d5e108f1fc9b9d27bba503fcac415cc751391cc
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-06-06 11:52:57 +0530

    updated

--- arrayyy/array1.java
+++ arrayyy/array1.java
@@ -5,6 +5,17 @@ public class array1 {
         String[] enames={"RG","SG","PG","NM"};
         System.out.println("length of entire array :"+ enames.length);
         System.out.println("1st index value string length :" + enames[0].length());
+
+        for(int i=0;i<enames.length;i++){
+            System.out.println(enames[i]);
+        }// for-loop
+
+        int i=0;
+        while (i<enames.length) {
+            System.out.println(enames[i]);
+            i++;
+        } // while-loop
+        
         for (String ename : enames) {
                 System.out.println(ename);
             
