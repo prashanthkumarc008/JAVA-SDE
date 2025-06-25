98a3fe64aea3f2bdb1e42748b7322bb61ce148c5
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-06-25 12:05:45 +0530

    updated

---
+++ java-practice/string1.java
@@ -0,0 +1,7 @@
+public class string1 {
+    public static void main(String[] args) {
+        String ename=new String("Rahul");
+        ename=ename.concat(" Gandhi");
+        System.out.println(ename);
+    }
+}
