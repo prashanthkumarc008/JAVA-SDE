cbec40d1463cb70f091daa022224e19ea1a88b0c
Prashanth Kumar C <prashanthkumarc008@gmail.com>
2025-10-17 22:39:03 +0530

    Update JDBCEX.java

--- JDBCEX.java
+++ JDBCEX.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JDBCEX {
+
     public static void main(String[] args) {
         // Database credentials and URL
         String url = "jdbc:mysql://localhost:3306/pacchu";
